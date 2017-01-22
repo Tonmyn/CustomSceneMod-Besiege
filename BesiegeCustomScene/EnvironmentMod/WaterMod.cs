987e153390745c3dda9261479c6fd2a680179573
panhao <4812ph@163.com>
2017-01-22 09:53:01 +0800

    fix bugs

--- BesiegeCustomScene/EnvironmentMod/WaterMod.cs
+++ BesiegeCustomScene/EnvironmentMod/WaterMod.cs
@@ -25,27 +25,20 @@ namespace BesiegeCustomScene
             ClearWater();
         }
         /// ////////////////////////      
-        string ScenePath = GeoTools.ScenePath;
         private GameObject[] Mwater;
         private int WaterSize = 0;
-        private Vector3 waterScale = new Vector3(1, 1, 1);
-        private Vector3 waterRepeat = new Vector3(0, 1, 0);
-        private Vector3 waterLocation = new Vector3(0, 0, 0);
         public void ReadScene(string SceneName)
         {
             WaterSize = 0;
-            waterScale = new Vector3(1, 1, 1);
-            waterRepeat = new Vector3(0, 1, 0);
-            waterLocation = new Vector3(0, 0, 0);
             try
             {
                 //  Debug.Log(Application.dataPath);
-                if (!File.Exists(ScenePath + SceneName + ".txt"))
+                if (!File.Exists(GeoTools.ScenePath + SceneName + ".txt"))
                 {
                     Debug.Log("Error! Scene File not exists!");
                     return;
                 }
-                StreamReader srd = File.OpenText(ScenePath + SceneName + ".txt");
+                StreamReader srd = File.OpenText(GeoTools.ScenePath + SceneName + ".txt");
                 while (srd.Peek() != -1)
                 {
                     string str = srd.ReadLine();
@@ -53,33 +46,61 @@ namespace BesiegeCustomScene
                     if (chara.Length > 2)
                     {
                         #region Water
-                        if (chara[0] == "Water")
+                        if (chara[0] == "MWater"|| chara[0] == "Mwater")
                         {
                             if (chara[1] == "size")
                             {
                                 this.WaterSize = Convert.ToInt32(chara[2]);
                                 LoadWater();
                             }
-                            else if (chara[1] == "repeat")
+                            if (chara[1] == "watertemp")
                             {
-                                waterRepeat = new Vector3(
-                                Convert.ToSingle(chara[2]),
-                                Convert.ToSingle(chara[3]),
-                                Convert.ToSingle(chara[4]));
+                                if (Convert.ToInt32(chara[2]) == 0)
+                                {
+                                    gameObject.GetComponent<Prop>().WaterTemp.SetActive(false);
+                                }
+                                else
+                                {
+                                    gameObject.GetComponent<Prop>().WaterTemp.SetActive(true);
+                                }
                             }
-                            else if (chara[1] == "mscale" || chara[1] == "scale")
+                        }
+                        else if (chara[0] == "Water")
+                        {
+                            int i = Convert.ToInt32(chara[1]);
+                            if (chara[2] == "mesh")
                             {
-                                waterScale = new Vector3(
-                                Convert.ToSingle(chara[2]),
-                                Convert.ToSingle(chara[3]),
-                                Convert.ToSingle(chara[4]));
+                                Mwater[i].GetComponent<MeshFilter>().mesh = GeoTools.MeshFromObj(chara[3]);
                             }
-                            else if (chara[1] == "location")
+                            else if (chara[2] == "wmesh")
                             {
-                                waterLocation = new Vector3(
-                                Convert.ToSingle(chara[2]),
-                                Convert.ToSingle(chara[3]),
-                                Convert.ToSingle(chara[4]));
+                                Mwater[i].GetComponent<MeshFilter>().mesh = GeoTools.WMeshFromObj(chara[3]);
+                            }
+                            else if (chara[2] == "scale")
+                            {
+                                Mwater[i].transform.localScale = new Vector3(
+                               Convert.ToSingle(chara[3]),
+                               Convert.ToSingle(chara[4]),
+                               Convert.ToSingle(chara[5]));
+                            }
+                            else if (chara[2] == "location")
+                            {
+                                Mwater[i].transform.localPosition = new Vector3(
+                               Convert.ToSingle(chara[3]),
+                               Convert.ToSingle(chara[4]),
+                               Convert.ToSingle(chara[5]));
+                            }
+                            else if (chara[2] == "meshcollider")
+                            {
+                                Mwater[i].GetComponent<MeshCollider>().sharedMesh = GeoTools.MeshFromObj(chara[3]);
+                                Mwater[i].GetComponent<MeshCollider>().convex = true;
+                                Mwater[i].GetComponent<MeshCollider>().isTrigger = true;
+                            }
+                            else if (chara[2] == "wmeshcollider")
+                            {
+                                Mwater[i].GetComponent<MeshCollider>().sharedMesh = GeoTools.WMeshFromObj(chara[3]);
+                                Mwater[i].GetComponent<MeshCollider>().convex = true;
+                                Mwater[i].GetComponent<MeshCollider>().isTrigger = true;
                             }
                         }
                         #endregion
@@ -101,30 +122,15 @@ namespace BesiegeCustomScene
             {
                 ClearWater();
                 if (WaterSize <= 0) return;
-                if (waterRepeat.x < 0) waterRepeat.x = 0;
-                if (waterRepeat.x > 9) waterRepeat.x = 9;
-                if (waterRepeat.z < 0) waterRepeat.z = 0;
-                if (waterRepeat.z > 9) waterRepeat.z = 9;
-                Mwater = new GameObject[((int)waterRepeat.x * 2 + 1) * ((int)waterRepeat.z * 2 + 1)];
-
-                //gameObject.GetComponent<Prop>().WaterTemp.SetActive(true);
-                int index = 0;
-                for (float k = -waterRepeat.x; k <= waterRepeat.x; k++)
-                {
-                    for (float i = -waterRepeat.z; i <= waterRepeat.z; i++)
-                    {
-                        Mwater[index] = Instantiate(gameObject.GetComponent<Prop>().TileTemp);
-                        Mwater[index].name = "water" + index.ToString();
-                        Mwater[index].transform.position = new Vector3((float)(
-                            k * 50 * waterScale.x) + waterLocation.x, waterLocation.y, (float)(i * 50 * waterScale.z) + waterLocation.z);
-                        Mwater[index].transform.localScale=waterScale;
-                        Mwater[index].SetActive(true);
-                        index++;
-                    }
+                Mwater = new GameObject[WaterSize];       
+                for (int i=0;i<Mwater.Length;i++)
+                {                
+                        Mwater[i] = Instantiate(gameObject.GetComponent<Prop>().TileTemp);
+                        Mwater[i].name = "water" + i.ToString();       
+                        Mwater[i].SetActive(true);
+                    Mwater[i].transform.localScale = new Vector3(1, 1, 1);
+                    Mwater[i].transform.localPosition = new Vector3(0, 0, 0);
                 }
-
-               // Mwater[0].AddComponent<PlanarReflection>();
-              //  Mwater[0].GetComponent<PlanarReflection>().m_ReflectionCamera = Camera.current;
             }
             catch (Exception ex)
             {
@@ -140,13 +146,13 @@ namespace BesiegeCustomScene
             if (Mwater.Length <= 0) return;
             Debug.Log("ClearWater");
             gameObject.GetComponent<Prop>().WaterTemp.SetActive(false);
+            try
+            {
+                Destroy(GameObject.Find("WaterTempReflectionMain Camera"));
+            }
+            catch { }
             for (int i = 0; i < Mwater.Length; i++)
             {
-                try
-                {
-                    Destroy(GameObject.Find("WaterTempReflectionMain Camera"));
-                }
-                catch { }
                 Destroy(Mwater[i]);
             }
         }
