b410dc033513c5c3ad7f1e663904a93f36ebbe1a
Patrick Xu <px.patrick@gmail.com>
2018-09-14 21:13:06 +1000

    minor language update

--- BesiegeCustomScene-for-0.6/Global/LanguageManager.cs
+++ BesiegeCustomScene-for-0.6/Global/LanguageManager.cs
@@ -1,9 +1,5 @@
 ﻿using Localisation;
-using Modding;
 using System;
-using System.Collections.Generic;
-//using System.IO;
-using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace BesiegeCustomScene
@@ -11,43 +7,43 @@ namespace BesiegeCustomScene
     public class LanguageManager : MonoBehaviour
     {
 
-        public static readonly bool isChinese = LocalisationManager.Instance.currLangName == "简体中文";
+        public static readonly bool isChinese = LocalisationManager.Instance.currLangName.Contains("中文");
 
         //Mini Map
-        public static String MiniMapTitle = isChinese ? "小地图" : "Mini Map";
+        public static string MiniMapTitle = isChinese ? "小地图" : "Mini Map";
 
         //Block Information
-        public static String BlockInformationTitle = isChinese ? "速度计" : "Speedometer";
+        public static string BlockInformationTitle = isChinese ? "速度计" : "Speedometer";
 
-        public static String AccelerationLabel = isChinese ? "加速度" : "Acceleration";
+        public static string AccelerationLabel = isChinese ? "加速度" : "Acceleration";
 
-        public static String VelocityLabel = isChinese ? "速度" : "Velocity";
+        public static string VelocityLabel = isChinese ? "速度" : "Velocity";
 
-        public static String PositionLabel = isChinese ? "位置" : "Position";
+        public static string PositionLabel = isChinese ? "位置" : "Position";
 
-        public static String DistanceLabel = isChinese ? "里程" : "Distance";
+        public static string DistanceLabel = isChinese ? "里程" : "Distance";
 
-        public static String GForceLabel = isChinese ? "过载" : "G-Force";
+        public static string GForceLabel = isChinese ? "过载" : "G-Force";
 
-        public static String TimerLabel = isChinese ? "计时器" : "Timer";
+        public static string TimerLabel = isChinese ? "计时器" : "Timer";
 
-        public static String TimeLabel = isChinese ? "时间" : "Time";
+        public static string TimeLabel = isChinese ? "时间" : "Time";
 
-        public static String TriggerLabel = isChinese ? "触发器" : "Trigger";
+        public static string TriggerLabel = isChinese ? "触发器" : "Trigger";
 
         //Scene Setting
-        public static String SceneWindowTitle = isChinese ? "地形设置" : "Scene Setting";
+        public static string SceneWindowTitle = isChinese ? "地形设置" : "Scene Setting";
 
-        public static String FogButtonLabel = isChinese ? "去掉迷雾" : "No Fog";
+        public static string FogButtonLabel = isChinese ? "去掉迷雾" : "No Fog";
 
-        public static String WorldBoundsButtonLabel = isChinese ? "去空气墙" : "No Bounds";
+        public static string WorldBoundsButtonLabel = isChinese ? "去空气墙" : "No Bounds";
 
-        public static String FloorGridButtonLabel = isChinese ? "去掉地板" : "No Floor";
+        public static string FloorGridButtonLabel = isChinese ? "去掉地板" : "No Floor";
 
-        public static String SceneListLabel = isChinese ? "地形列表" : "Scenes List";
+        public static string SceneListLabel = isChinese ? "地形列表" : "Scenes List";
 
-        public static String ReloadButonLabel = isChinese ? "重载地图" : "Reload Scene";
+        public static string ReloadButonLabel = isChinese ? "重载地图" : "Reload Scene";
 
-        public static String ScenesDirectoryButtonLabel = isChinese ? "打开地图文件夹" : "Scenes Folder";
+        public static string ScenesDirectoryButtonLabel = isChinese ? "打开地图文件夹" : "Scenes Folder";
     }
 }
